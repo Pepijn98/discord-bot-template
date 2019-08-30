eb79990845cb94378d8b71cfa9ef37556d57b7dc
KurozeroPB <root@kurozeropb.info>
2019-08-30 09:26:35 +0200

    Add example settings

--- settings.ts
+++
@@ -1,9 +0,0 @@
-import { ISettings } from "./src/interfaces/ISettings";
-
-const settings: ISettings = {
-    token: "NjE1ODgzNTQ3MjMxOTExOTM2.XWUgUg.GHWVsDHVlY6CTwddwtArlE1acC8",
-    owner: "93973697643155456",
-    prefix: "m/"
-};
-
-export default settings;
